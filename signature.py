2b1105d44dc7d1360d40b361e922e55cc248ef3b
Om Rawal <omrawal007@gmail.com>
2021-11-07 12:07:34 +0530

    Comparison module complete

---
+++ signature.py
@@ -0,0 +1,28 @@
+import cv2
+from skimage.metrics import structural_similarity as ssim
+
+# TODO add contour detection for enhanced accuracy
+
+
+def match(path1, path2):
+    # read the images
+    img1 = cv2.imread(path1)
+    img2 = cv2.imread(path2)
+    # turn images to grayscale
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # resize images for comparison
+    img1 = cv2.resize(img1, (300, 300))
+    img2 = cv2.resize(img2, (300, 300))
+    # display both images
+    cv2.imshow("One", img1)
+    cv2.imshow("Two", img2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    print(ssim(img1, img2))
+    similarity_value = "{:.2f}".format(ssim(img1, img2)*100)
+    print("answer is ", similarity_value, "type=", type(similarity_value))
+
+
+match("D:\\Code\\Git stuff\\Signature-Matching\\assets\\1.png",
+      "D:\\Code\\Git stuff\\Signature-Matching\\assets\\3.png")
